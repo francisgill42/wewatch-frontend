515d5e60c1a8f62d30fd007635be95c3b6cce8a1
FIRST_NAME LAST_NAME <francisgill42@gmail.com>
2021-03-02 14:38:53 +0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,165 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+    class="primary"
+    dark
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action >
+            <v-icon >{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      
+      class="white"
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <!-- <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>mdi-application</v-icon>
+      </v-btn> -->
+      <!-- <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>mdi-minus</v-icon>
+      </v-btn> -->
+      <!-- <v-toolbar-title v-text="title" /> -->
+        <img src="https://wewatch.ae/wp-content/uploads/2020/03/logo-web.png" />
+      <v-spacer />
+      <v-btn @click="logout" icon color="secondary">
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer 
+      dark
+      class="grey"
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Report',
+          to: '/report'
+        },
+       
+         {
+          icon: 'mdi-chart-bubble',
+          title: 'wewatch Manager',
+          to: '/wewatchmanager'
+        },
+         {
+          icon: 'mdi-chart-bubble',
+          title: 'User',
+          to: '/User'
+        },
+
+        // {
+        //   icon: 'mdi-chart-bubble',
+        //   title: 'User',
+        //   to: '/User'
+        // },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Project Admin',
+          to: '/project_admin'
+         },
+
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Client',
+          to: '/client'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Project',
+          to: '/project'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Allocation',
+          to: '/allocations'
+        },       
+        
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'wewatch.ae'
+    }
+  },
+  methods:{
+     logout() {
+       this.$auth.logout();
+},
+  }
+}
+</script>
---
+++ pages/allocations/_id.vue
@@ -0,0 +1,156 @@
+<template>
+  <v-container>
+    
+    <v-row>
+        <v-col cols="12">
+        <v-card class="pa-2">
+        <v-col
+        cols="12"
+        sm="12"
+        >
+        <v-select
+        v-model="project_id"
+        :items="projects"
+        item-value="id"
+        item-text="project_name"
+        :menu-props="{ maxHeight: '400' }"
+        label="Select project"
+        ></v-select>
+        </v-col>
+        
+
+        <v-row>
+        <v-col cols="12" class="pa-5" md="3" sm="12">
+        Managers
+        <v-checkbox 
+        @change="for_manager_ids"
+        v-model="check_all_manager_ids"
+        label="Select All"
+        ></v-checkbox>
+
+         <v-checkbox v-for="(item,i) in managers" :key="i"
+        v-model="manager_ids"
+        :label="item.name"
+        :value="item.id"
+        ></v-checkbox>
+
+        </v-col>
+
+        <v-col cols="12" class="pa-5" md="3" sm="12">
+        Users
+        <v-checkbox 
+        @change="for_user_ids" 
+        v-model="check_all_user_ids" 
+        label="Select All"
+        ></v-checkbox>
+
+        <v-checkbox v-for="(item,i) in users" :key="i"
+        v-model="user_ids"
+        :label="item.name"
+        :value="item.id"
+        ></v-checkbox>
+
+        </v-col>
+
+        </v-row>
+        <v-col
+        cols="12"
+        sm="12"
+        >
+        <v-btn class="secondary" small @click="submit">Allocate Users</v-btn>
+        </v-col>
+        </v-card>
+
+        </v-col>
+         
+    </v-row>
+
+  </v-container>
+
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        check_all_user_ids : false,
+        check_all_manager_ids : false,
+      
+        project_id: Number,
+        user_ids : [],
+        manager_ids : [],
+        data : [],
+        projects : [],
+        users : [],
+        managers : [],
+
+      }
+    },
+    computed:{
+      
+    },
+  
+    created (){
+        this.$axios.get('/allocation/' + this.$route.params.id)
+            .then((res) => {
+              this.manager_ids = res.data.manager_ids;
+              this.user_ids = res.data.user_ids;
+              this.project_id = res.data.project_id;
+              console.log(this.$route.params.id,res.data);
+                
+            });
+             this.$axios.get('/project')
+            .then((res) => {
+                this.projects = res.data;
+            });
+            this.$axios.get(`get_users_by_id/${5}`)
+            .then(res => {
+            this.users = res.data.data;
+            });
+
+            this.$axios.get(`get_users_by_id/${4}`)
+            .then(res => {
+            this.managers = res.data.data;
+            });
+
+    },
+    methods:{
+
+        for_user_ids(){
+          this.user_ids = this.check_all_user_ids ? this.users.map(v => v.id) : [] ;
+        },
+        for_manager_ids(){
+          this.manager_ids = this.check_all_manager_ids ? this.managers.map(v => v.id) : [] ;
+        },
+       
+        submit(){
+            let payload = {
+                project_id : this.project_id,
+                user_ids :   this.user_ids,
+                manager_ids : this.manager_ids
+            };
+
+          
+
+            this.$axios.put('allocation/' + this.$route.params.id, payload)
+            .then(res => {
+
+                    this.msg = '';
+
+                    if(res.data.success){
+                      this.msg = 'record has been inserted';
+                      this.data.unshift(res.data.data); 
+                    }
+                    else{
+                      this.msg = 'record cannot insert';
+                    }
+
+                    this.snackbar = true;
+          
+            })
+            .catch(err => console.log(err));
+        }
+    }
+  }
+</script>
